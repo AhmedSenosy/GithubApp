102d0fdda4fcfe0421e48421fc4c3be65d0f565a
Ahmed <ahmedsenosy.1994@gmail.com>
2022-01-24 02:55:13 +0200

    - Add dependencies to Domain - add base class to completable and observable

---
+++ Domain/src/main/java/com/senosy/domain/interactor/CompletableUseCase.kt
@@ -0,0 +1,32 @@
+package com.senosy.domain.interactor
+
+import com.senosy.domain.executor.PostExecutionThread
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.observers.DisposableCompletableObserver
+import io.reactivex.rxjava3.observers.DisposableObserver
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+abstract class CompletableUseCase< in Params> constructor(
+    private val postExecutionThread: PostExecutionThread
+) {
+    private val disposables = CompositeDisposable()
+
+    protected abstract fun buildUseCaseObservable(params: Params? = null): Completable
+
+    open fun execute(observer: DisposableCompletableObserver, params: Params? = null) {
+        val completable = this.buildUseCaseObservable(params).subscribeOn(Schedulers.io())
+            .observeOn(postExecutionThread.scheduler)
+        addObservable(observable.subscribeWith(completable))
+    }
+
+    fun dispose() {
+        disposables.dispose()
+    }
+
+    private fun addObservable(disposable: Disposable) {
+        disposables.add(disposable)
+    }
+}
